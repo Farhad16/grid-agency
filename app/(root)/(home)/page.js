fc11cc25892550e8aaf5afaa60822fff51477df3
Farhad16 <fhossain389@gmail.com>
2023-12-29 16:30:00 +0600

    Image animation position in center

--- app/(root)/(home)/page.js
+++ app/(root)/(home)/page.js
@@ -5,12 +5,12 @@ const Page = () => {
   return (
     <div className="flex flex-col text-white bg-[#0A0808] min-h-screen relative">
       <Loading />
-      <ReusableImageAnimation imageSrc="text1.png" imageAlt="text1" />
-      <ReusableImageAnimation imageSrc="text2.png" imageAlt="text2" />
-      <ReusableImageAnimation imageSrc="text3.png" imageAlt="text3" />
-      <ReusableImageAnimation imageSrc="text4.png" imageAlt="text4" />
-      <ReusableImageAnimation imageSrc="text5.png" imageAlt="text5" />
-      <ReusableImageAnimation imageSrc="text6.png" imageAlt="text6" />
+      <ReusableImageAnimation imageSrc="text1.png" imageAlt="text1" scale={5} />
+      <ReusableImageAnimation imageSrc="text2.png" imageAlt="text2" scale={5} />
+      <ReusableImageAnimation imageSrc="text3.png" imageAlt="text3" scale={5} />
+      <ReusableImageAnimation imageSrc="text4.png" imageAlt="text4" scale={5} />
+      <ReusableImageAnimation imageSrc="text5.png" imageAlt="text5" scale={5} />
+      <ReusableImageAnimation imageSrc="text6.png" imageAlt="text6" scale={2} />
     </div>
   );
 };

8bc74840d4dcdeb7ca48868a31bc7251a3975d12
Farhad16 <fhossain389@gmail.com>
2024-01-03 12:34:37 +0600

    Footer section responsive done

--- components/shared/Footer.js
+++ components/shared/Footer.js
@@ -19,27 +19,30 @@ const Footer = () => {
       }}
     >
       <Wrapper className="w-full flex flex-col sm:mt-[80px] mt-24">
-        <div className="flex flex-col md:flex-row items-center lg:justify-between sm:gap-20 gap-12 py-10">
+        <div className="flex flex-col-reverse md:flex-row items-center lg:justify-between lg:gap-20 gap-12 py-10">
           <img
             src="/assets/footer/footer.gif"
             layout="responsive"
-            className="w-[308px]"
+            className="lg:w-[308px] md:w-[250px] w-[175px]"
             alt="footergif"
           />
-          <div className="flex flex-col gap-2 w-full md:w-fit">
-            <p className="bg-yellow-550 rotate-[2deg] w-fit inline-block text-sm sm:text-[29px] text-[#231F20] px-4 sm:py-2 mb-2 font-bold">
-              new business
-            </p>
-            <h1 className="text-light-50 md:text-[70px] lg:text-[100px] text-4xl font-bold sm:leading-[90px]">
-              <span>have a</span> <span>stupid</span>
-            </h1>
-            <div className="flex lg:flex-row flex-col sm:gap-8 gap-6 sm:mt-8 mt-0">
-              <p className="text-light-50 md:text-[70px] lg:text-[100px] text-4xl font-bold">
+          <div className="flex flex-col w-full md:w-fit">
+            <div className="flex flex-col">
+              <p className="bg-yellow-550 rotate-[2deg] w-fit inline-block text-sm sm:text-[29px] text-[#231F20] px-4 sm:py-2 -mb-2 font-bold">
+                new business
+              </p>
+              <h1 className="text-light-50 md:text-[60px] lg:text-[80px] xl:text-[100px] text-5xl font-bold lg:leading-[90px]">
+                <span>have a</span> <span>stupid</span>
+              </h1>
+            </div>
+
+            <div className="flex flex-row items-center sm:gap-8 gap-6 mt-2">
+              <p className="text-light-50 md:text-[60px] lg:text-[80px] xl:text-[100px] text-5xl font-bold">
                 idea?
               </p>
               <Link
                 href="/contact"
-                className="focus:outline-none outline-none group flex items-center border border-2 rounded-full border-yellow-550 transition duration-300 lg:w-[350px] w-full h-[50px]"
+                className="focus:outline-none outline-none group flex items-center border border-2 rounded-full border-yellow-550 transition duration-300 md:w-[240px] lg:w-[300px] xl:w-[350px] w-full lg:h-[50px] lg:h-[45px] h-[40px]"
               >
                 <p className="w-full flex items-center group-hover:justify-center">
                   <span className="mr-4 uppercase opacity-0 group-hover:opacity-100 transition-opacity transition-transform transform -translate-x-full group-hover:translate-x-0 text-light-50">
@@ -56,8 +59,8 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div className="sm:tracking-[3px] flex sm:flex-row flex-col sm:flex-wrap sm:justify-between sm:items-center sm:mt-[200px] mt-[100px] text-xs sm:text-[15px] gap-4">
-          <div className="uppercase flex flex-col text-light-50 gap-1">
+        <div className="sm:tracking-[3px] flex flex-row justify-between items-center sm:mt-[150px] mt-[50px] text-[10px] md:text-[15px] gap-4">
+          <div className="uppercase sm:flex flex-col text-light-50 gap-1 hidden">
             <p>DO YOU LIKE STUPID IDEAS?</p>
             <p className="underline">jointheteam@gridagenyinc.ca</p>
           </div>
@@ -65,22 +68,26 @@ const Footer = () => {
             <p>242 Applewood Cres</p>
             <p>Concord, ON L4K 4E5</p>
           </div>
+          <div className="items-center gap-2 text-light-50 sm:hidden">
+            <InstagramIcon />
+            <LinkedInIcon />
+          </div>
           <div className="flex flex-row gap-4 justify-between sm:justify-start">
             <div className="uppercase flex flex-col text-light-50 sm:items-end gap-1">
               <p className="text-left underline">info@gridagenyinc.ca</p>
               <p className="text-left underline">(416) 557-4773</p>
             </div>
-            <div className="flex items-center gap-2 text-light-50">
+            <div className="sm:flex items-center gap-2 text-light-50 hidden">
               <InstagramIcon />
               <LinkedInIcon />
             </div>
           </div>
         </div>
-        <div className="w-full flex flex-col sm:flex-row justify-between pt-10 border-t border-1 border-light-50 border-dotted mt-[80px] gap-1">
-          <p className="text-light-50 text-light-50 text-[10px]">
+        <div className="uppercase w-full flex flex-col sm:flex-row justify-between sm:pt-10 pt-6 border-t border-1 border-light-50 border-dotted sm:mt-[80px] mt-[40px] gap-1">
+          <p className="text-light-50 text-light-50 text-[10px] sm:text-left text-center">
             © 2023, Grid Creative Advertising Inc.
           </p>
-          <p className="text-light-50 text-light-50 text-[10px]">
+          <p className="text-light-50 text-light-50 text-[10px] sm:text-left text-center">
             privacy and cookie policy
           </p>
         </div>

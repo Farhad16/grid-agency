7ee7b4fa3d6696c45d7d1107fbad39f5c409f526
Farhad16 <fhossain389@gmail.com>
2024-01-18 10:15:19 +0600

    Remove view case from blog

--- components/blogs/BlogItem.js
+++ components/blogs/BlogItem.js
@@ -1,55 +1,11 @@
-"use client";
-import React, { useEffect, useState, useRef } from "react";
 import Link from "next/link";
-import CursorView from "./CursorView";
 import HTMLParser from "../shared/HTMLParser";
 
 const BlogItem = ({ blog }) => {
-  const [cursorPosition, setCursorPosition] = useState({ x: 300, y: 300 });
-  const [isHovered, setIsHovered] = useState(false);
-  const itemRef = useRef();
-
-  const handleMouseMove = (e) => {
-    setCursorPosition({ x: e.clientX, y: e.clientY });
-  };
-
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-  };
-
-  useEffect(() => {
-    const item = itemRef.current;
-
-    const handleScroll = () => {
-      const rect = item.getBoundingClientRect();
-      const isInViewport = rect.top >= 0 && rect.bottom <= window.innerHeight;
-
-      if (isInViewport) {
-        handleMouseEnter();
-      } else {
-        handleMouseLeave();
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
     <Link
       href={`/blogs/${blog.slug}`}
       className="flex flex-col gap-4 relative hover:no-underline no-underline"
-      onMouseOver={handleMouseEnter}
-      onMouseMove={handleMouseMove}
-      onMouseOut={handleMouseLeave}
-      ref={itemRef}
     >
       <img
         src={`${process.env.NEXT_PUBLIC_IMAGE_BASE_URL}/${blog.featured_image}`}
@@ -65,7 +21,6 @@ const BlogItem = ({ blog }) => {
           <HTMLParser content={blog.description} />
         </div>
       </div>
-      <CursorView cursorPosition={cursorPosition} isHovered={isHovered} />
     </Link>
   );
 };

554c228cd83575d8db023e8ea933795c76305620
Farhad16 <fhossain389@gmail.com>
2024-01-20 01:51:38 +0600

    Blog section design issue fixed

--- components/main/Blogs.js
+++ components/main/Blogs.js
@@ -1,16 +1,61 @@
-import React from "react";
-import ScrollTalkMobile from "./ScrollTalkMobile";
-import BlogScroll from "./BlogScroll";
+import React, { useEffect, useState } from "react";
+import { getServices } from "@/apis/service.api";
+import NoDataFound from "../shared/NoDataFound";
+import { CircularProgress } from "@mui/material";
+import BlogsDesktop from "./BlogsDesktop";
+import { talkData } from "@/constance/talks.data";
 
-const Blogs = () => {
+const Services = () => {
+  const [loading, setLoading] = useState(true);
+  const [serviceData, setServiceData] = useState([]);
+
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  const updateScreenWidth = () => {
+    setScreenWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    updateScreenWidth();
+
+    window.addEventListener("resize", updateScreenWidth);
+
+    return () => {
+      window.removeEventListener("resize", updateScreenWidth);
+    };
+  }, [screenWidth]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getServices();
+        setServiceData(data);
+      } catch (error) {
+        console.error("Error fetching services:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="bg-[#241f20] border-top border-bottom pt-20 relative">
-      <div className="w-full h-full flex items-center justify-center">
-        <BlogScroll />
-        <ScrollTalkMobile />
-      </div>
+    <div className="w-full h-full flex items-center justify-center bg-[#241f20]">
+      {loading ? (
+        <CircularProgress
+          className="text-light-50 mb-[100px]"
+          style={{ color: "#E6E0D2" }}
+        />
+      ) : talkData.length > 0 ? (
+        <>
+          <BlogsDesktop blogData={talkData} screenWidth={screenWidth} />
+        </>
+      ) : (
+        <NoDataFound data="service" className="!text-black" />
+      )}
     </div>
   );
 };
 
-export default Blogs;
+export default Services;
---
+++ components/main/BlogsDesktop.js
@@ -0,0 +1,126 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect, useRef } from "react";
+import VerticleEl from "../shared/VerticleEl";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const BlogsDesktop = ({ blogData, screenWidth }) => {
+  const sectionRef = useRef(null);
+  const triggerRef = useRef(null);
+
+  function getScreenWidth() {
+    if (screenWidth > 1900) {
+      return "-60vw";
+    } else if (screenWidth >= 1600 && screenWidth <= 1900) {
+      return "-100vw";
+    } else if (screenWidth > 1400 && screenWidth < 1600) {
+      return "-120vw";
+    } else if (screenWidth >= 900 && screenWidth <= 1400) {
+      return "-185vw";
+    } else if (screenWidth >= 700 && screenWidth < 900) {
+      return "-225vw";
+    } else if (screenWidth < 700) {
+      return "-290vw";
+    }
+  }
+
+  useEffect(() => {
+    const pin = gsap.fromTo(
+      sectionRef.current,
+      {
+        translateX: 0,
+      },
+      {
+        translateX: `${getScreenWidth()}`,
+        ease: "none",
+        duration: 1,
+        scrollTrigger: {
+          trigger: triggerRef.current,
+          start: `top top+=50`,
+          end: "2000 top",
+          scrub: 0.6,
+          pin: true,
+        },
+      }
+    );
+    return () => {
+      pin.kill();
+    };
+  }, []);
+
+  const router = useRouter();
+  const workRoute = () => {
+    router.push("/blogs");
+  };
+
+  return (
+    <section className="overflow-hidden relative">
+      <div ref={triggerRef}>
+        <div ref={sectionRef} className="flex relative flex-row">
+          <div className="flex background-text xl:pr-[300px] pt-[50px] items-center justify-center">
+            <div className="flex gap-72 mr-[300px] px-[150px]">
+              <VerticleEl className="sm:-left-[3%] sm:top-[30%] md:left-[0%] lg:left-[0%] !text-light-50 z-10">
+                STUPID TALKS
+              </VerticleEl>
+              {blogData.map((talk, i) => (
+                <div
+                  key={talk.id}
+                  className="relative w-[350px] h-[500px] sm:w-[450px] sm:h-[500px] md:w-[500px] md:h-[600px] lg:w-[710px] lg:h-[800px] mb-[50px] z-10"
+                  style={{
+                    backgroundImage: `#cdcdcd`,
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "100% auto",
+                    backgroundPosition: "top",
+                  }}
+                >
+                  <div
+                    className={`flex flex-row lg:min-w-[900px] sm:min-w-[750px] min-w-[600px] z-10 pt-2 h-[100vh] ${
+                      i % 2 === 0 ? "items-start" : "items-end"
+                    }`}
+                    key={i}
+                  >
+                    <div className="flex gap-6 items-start">
+                      <span className="font-extrabold text-lg sm:text-[40px] text-left flex items-end justify-end text-yellow-550">
+                        {talk.serial}
+                      </span>
+                      <div className="relative">
+                        <img
+                          className="sm:max-w-[450px] lg:max-w-[600px] 4xl:max-w-[700px] h-[250px] sm:h-[280px] lg:h-[385px] 4xl:h-[425px] rounded-xl"
+                          src={talk.img}
+                          alt="img"
+                        />
+                        <Link
+                          href={`/blogs/${talk.id}`}
+                          className="hover:no-underline no-underline hover:text-light-50 font-extrabold absolute text-4xl sm:text-[60px] lg:text-[70px] text-light-50 top-[40%] sm:-right-[200px] -right-[100px] sm:leading-[70px] tracking-[-3.5px] hover:bg-yellow-600 transition duration-300 ease px-8"
+                        >
+                          {talk.about}
+                        </Link>
+                      </div>
+                      <p className="text-sm md:text-base lg:text-lg font-normal tracking-widest ml-8 w-[140px]">
+                        {talk.date}
+                      </p>
+                    </div>
+                    {i === blogData.length - 1 && (
+                      <p
+                        onClick={workRoute}
+                        className="sm:mt-40 lg:mt-80 cursor-pointer gap-4 -rotate-90 font-extralight text-xs sm:text-[21px] tracking-[6.93px] !text-yellow-550 z-10 flex flex-row"
+                      >
+                        <span>READ </span> <span>MORE</span>
+                      </p>
+                    )}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default BlogsDesktop;

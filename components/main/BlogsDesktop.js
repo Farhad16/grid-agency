01f833750c85963ed61d4e978eec91fb2a7d083e
Farhad16 <fhossain389@gmail.com>
2024-01-26 23:02:13 +0600

    Blog section modify

--- components/main/BlogsDesktop.js
+++ components/main/BlogsDesktop.js
@@ -9,11 +9,11 @@ import VerticleEl from "../shared/VerticleEl";
 gsap.registerPlugin(ScrollTrigger);
 
 const BlogsDesktop = ({ blogData }) => {
-  const sectionRef = useRef(null);
-  const triggerRef = useRef(null);
+  const blogSection = useRef(null);
+  const blogTarget = useRef(null);
 
   useEffect(() => {
-    const races = sectionRef.current;
+    const races = blogSection.current;
 
     function getScrollAmount() {
       let racesWidth = races.scrollWidth;
@@ -21,7 +21,7 @@ const BlogsDesktop = ({ blogData }) => {
     }
 
     const pin = gsap.fromTo(
-      sectionRef.current,
+      blogSection.current,
       {
         translateX: 0,
       },
@@ -30,7 +30,7 @@ const BlogsDesktop = ({ blogData }) => {
         ease: "none",
         duration: 1,
         scrollTrigger: {
-          trigger: triggerRef.current,
+          trigger: blogTarget.current,
           start: `top top+=50`,
           end: "2000 top",
           scrub: 0.6,
@@ -51,62 +51,48 @@ const BlogsDesktop = ({ blogData }) => {
 
   return (
     <section className="overflow-hidden relative">
-      <div ref={triggerRef}>
-        <div ref={sectionRef} className="flex relative flex-row">
-          <div className="flex background-text pt-[50px] items-center justify-center px-[100px]">
+      <div ref={blogTarget}>
+        <div ref={blogSection} className="flex relative flex-row">
+          <div className="flex pt-[50px] items-center justify-center px-[100px]">
             <div className="flex gap-56 xl:gap-60 z-10">
-              <VerticleEl className="sm:-left-[3%] sm:top-[30%] md:left-[0%] lg:left-[0%] !text-light-50 z-10">
+              <VerticleEl className="-left-[2%] top-[50%] z-10">
                 STUPID TALKS
               </VerticleEl>
               {blogData.map((talk, i) => (
-                <div
-                  key={talk.id}
-                  className="relative w-[350px] h-[500px] sm:w-[450px] sm:h-[500px] md:w-[500px] md:h-[600px] lg:w-[710px] lg:h-[800px] 5xl:w-[850px] 5xl:h-[900px] mb-[50px]"
-                  style={{
-                    backgroundSize: "cover",
-                    backgroundRepeat: "no-repeat",
-                    backgroundSize: "100% auto",
-                    backgroundPosition: "top",
-                  }}
-                >
+                <div key={talk.id} className="relative mb-[50px] min-h-screen">
                   <div
-                    className={`flex flex-row z-10 pt-2 h-full`}
-                    //  ${
-                    // i % 2 === 0
-                    //   ? "items-start"
-                    //   : "items-end -mt-[100px] lg:-mt-[150px]"
-                    // }`}
+                    className={`flex gap-6 items-start ${
+                      i % 2 === 0 ? "mt-0" : "mt-[100px] lg:mt-[150px]"
+                    }`}
                   >
-                    <div className="flex gap-6 items-start">
-                      <span className="font-extrabold text-lg sm:text-[40px] text-left flex items-end justify-end text-yellow-550">
-                        0{i + 1}
-                      </span>
-                      <div className="relative">
-                        <img
-                          className="max-w-[450px] lg:max-w-[600px] 4xl:max-w-[750px] h-[250px] sm:h-[280px] lg:h-[385px] 4xl:h-[500px] rounded-xl"
-                          src={`${process.env.NEXT_PUBLIC_IMAGE_BASE_URL}/${talk.featured_image}`}
-                          alt="img"
-                        />
-                        <Link
-                          href={`/blogs/${talk.slug}`}
-                          className="hover:no-underline no-underline active:bg-yellow-600 hover:text-light-50 font-extrabold absolute text-4xl sm:text-[60px] lg:text-[70px] 4xl:text-[80px] text-light-50 top-[40%] -right-[20%] sm:-right-[30%] lg:-right-[40%] w-[100%] sm:leading-[70px] tracking-[-3.5px] hover:bg-yellow-600 transition duration-300 ease px-8"
-                        >
-                          {talk.title}
-                        </Link>
-                      </div>
-                      <p className="text-sm md:text-base lg:text-lg font-normal tracking-widest ml-8 w-[140px]">
-                        {dayjs(talk.date).format("DD MMM YYYY")}
-                      </p>
-                    </div>
-                    {i === blogData.length - 1 && (
-                      <p
-                        onClick={workRoute}
-                        className="-rotate-90 absolute cursor-pointer gap-4 -right-[35%] top-[60%] font-extralight text-xs sm:text-[21px] tracking-[6.93px] !text-yellow-550 z-10 flex flex-row"
+                    <span className="font-extrabold text-lg sm:text-[40px] text-left flex items-end justify-end text-yellow-550">
+                      0{i + 1}
+                    </span>
+                    <div className="relative">
+                      <img
+                        className="max-w-[450px] lg:max-w-[600px] 4xl:max-w-[750px] h-[250px] sm:h-[280px] lg:h-[385px] 4xl:h-[500px] rounded-xl"
+                        src={`${process.env.NEXT_PUBLIC_IMAGE_BASE_URL}/${talk.featured_image}`}
+                        alt="img"
+                      />
+                      <Link
+                        href={`/blogs/${talk.slug}`}
+                        className="hover:no-underline no-underline active:bg-yellow-600 hover:text-light-50 font-extrabold absolute text-4xl sm:text-[60px] lg:text-[70px] 4xl:text-[80px] text-light-50 top-[40%] -right-[20%] sm:-right-[30%] lg:-right-[40%] w-[100%] sm:leading-[70px] tracking-[-3.5px] hover:bg-yellow-600 transition duration-300 ease px-8"
                       >
-                        <span>READ </span> <span>MORE</span>
-                      </p>
-                    )}
+                        {talk.title}
+                      </Link>
+                    </div>
+                    <p className="text-sm md:text-base lg:text-lg font-normal tracking-widest ml-8 w-[140px]">
+                      {dayjs(talk.date).format("DD MMM YYYY")}
+                    </p>
                   </div>
+                  {i === blogData.length - 1 && (
+                    <VerticleEl
+                      className="-right-[12%] top-[40%] z-10 !text-yellow-550"
+                      onClick={workRoute}
+                    >
+                      READ MORE
+                    </VerticleEl>
+                  )}
                 </div>
               ))}
             </div>
